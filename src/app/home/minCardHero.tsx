13e6d9ea89cdc0d0e1b9d09aa7b385ee5c59212b
Madonna Adel <madonnadel0@gmail.com>
2024-09-28 21:14:55 +0300

    add home

--- src/app/home/minCardHero.tsx
+++ src/app/home/minCardHero.tsx
@@ -17,7 +17,7 @@ const MinCardHeroSection: React.FC<MinCardHeroSectionProps> = ({
       <div className="bg-secondary p-4 rounded-full mb-4 w-16">
         <Image src={icon} alt="feature Imge" width={40} height={40} />
       </div>
-      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <h1 className="text-xl font-semibold mb-2">{title}</h1>
       <p className=" text-gray-300">{description}</p>
     </div>
   );
