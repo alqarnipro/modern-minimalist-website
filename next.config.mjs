94c05a7afc5942daf178b07b0e1bb3a86b1f2f63
Madonna Adel <madonnadel0@gmail.com>
2024-09-30 05:26:53 +0300

    services page

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    modern: true, 
-  },
-  future: {
-    webpack5: true, 
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
