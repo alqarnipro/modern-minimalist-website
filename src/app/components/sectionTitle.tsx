38e4a874cf96f890b06a76b68d4676f3f7ae3902
Madonna Adel <madonnadel0@gmail.com>
2024-09-30 03:14:53 +0300

    services page

--- src/app/components/sectionTitle.tsx
+++ src/app/components/sectionTitle.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 
 interface SectionTitleProps {
@@ -10,12 +11,14 @@ const SectionTitle: React.FC<SectionTitleProps> = ({
   underlineWidth = "1/4",
 }) => {
   return (
-    <div className="flex align-middle justify-start">
-      <p className="m-0 p-0 text-sm text-gray-500">{text}</p>
-      <div
-        className={`left-0 bottom-1 w-${underlineWidth} h-0.5 bg-gray-700 mt-3 ms-2`}
-      ></div>
-    </div>
+    <Head>
+      <div className="flex align-middle justify-start">
+        <title className="m-0 p-0 text-sm text-gray-500">{text}</title>
+        <div
+          className={`left-0 bottom-1 w-${underlineWidth} h-0.5 bg-gray-700 mt-3 ms-2`}
+        ></div>
+      </div>
+    </Head>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navBar";
 import Footer from "./components/footer";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,13 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Digital Marketing Agency - Welcome to MARKETEER</title>
+        <meta
+          name="description"
+          content="This is a detailed description of the home page content."
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div data-aos="fade-up">
           <Navbar />
         </div>
-
         {children}
         <div data-aos="fade-right">
           <Footer />
