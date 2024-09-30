b70ad8ead29d4fdf95efb1c5c985c27ca3446093
Madonna Adel <madonnadel0@gmail.com>
2024-09-30 03:27:51 +0300

    services page

--- src/app/components/sectionTitle.tsx
+++ src/app/components/sectionTitle.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import React from "react";
 
 interface SectionTitleProps {
@@ -11,14 +10,12 @@ const SectionTitle: React.FC<SectionTitleProps> = ({
   underlineWidth = "1/4",
 }) => {
   return (
-    <Head>
-      <div className="flex align-middle justify-start">
-        <title className="m-0 p-0 text-sm text-gray-500">{text}</title>
-        <div
-          className={`left-0 bottom-1 w-${underlineWidth} h-0.5 bg-gray-700 mt-3 ms-2`}
-        ></div>
-      </div>
-    </Head>
+    <div className="flex align-middle justify-start">
+      <p className="m-0 p-0 text-sm text-gray-500">{text}</p>
+      <div
+        className={`left-0 bottom-1 w-${underlineWidth} h-0.5 bg-gray-700 mt-3 ms-2`}
+      ></div>
+    </div>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({
           name="description"
           content="Mary's simple recipe for maple bacon donuts
            makes a sticky, sweet treat with just a hint
-           of salt that you'll keep coming back for."/>
+           of salt that you'll keep coming back for."
+        />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
